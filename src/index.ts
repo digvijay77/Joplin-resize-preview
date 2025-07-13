54270464b2867f3216e18a7c803119330533a67a
Digvijay Bedkyale <digvijaybedkyale07@gmail.com>
2025-07-13 13:47:48 +0530

    Removed commented code

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,10 @@ joplin.plugins.register({
   onStart: async function() {
         console.info('Resizable Panes plugin started!');
 
-        // Register the content script and stylesheet
         await joplin.contentScripts.register(
-            ContentScriptType.CodeMirrorPlugin, // This type targets the editor area
+            ContentScriptType.CodeMirrorPlugin, 
             'resizable-panes-script',
             'webview-scripts/resizable.js'
         );
-
-        // await joplin.contentScripts.register(
-        //     ContentScriptType.CodeMirrorPlugin,
-        //     'resizable-panes-style',
-        //     'webview-scripts/style.css'
-        // );
     },
 });
